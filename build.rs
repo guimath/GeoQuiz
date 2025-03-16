e3acf9e381b4ae8b08a64d26615c547bde07d8a0
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2025-03-16 21:41:20 +0100

    added style

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    slint_build::compile("ui/app-window.slint").expect("Slint build failed");
+    let config =
+    slint_build::CompilerConfiguration::new()
+    .with_style("material-dark".into());
+    slint_build::compile_with_config("ui/app-window.slint", config).unwrap();
+    // slint_build::compile("ui/app-window.slint").expect("Slint build failed");
 }
